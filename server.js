cf16022f213c7a878be1d884ae0f8337612af563
yousuf1318 <61650368+yousuf1318@users.noreply.github.com>
2021-07-10 11:25:16 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const connectDB = require("./config/db");
+// const path = require("path");
+const app = express();
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API Running"));
+
+//Routes
+app.use("/api/user", require("./routes/api/user")); //For user sign up
+app.use("/api/auth", require("./routes/api/auth")); //For user sign in
+app.use("/api/travelagency", require("./routes/api/travelAgency")); //Fro traveler sign up
+app.use("/api/travelagency", require("./routes/api/travelAgencyAuth")); //Fro traveler sign in
+app.use("/api/buses", require("./routes/api/bus")); // To get all the buses
+app.use("/api/ticket", require("./routes/api/ticket")); // For cancle/book tickets
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+
+
